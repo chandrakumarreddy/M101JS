140122e429b2ea64a84d719a797426dc5c4aaef1
chandrakumarreddy <chandrapenugonda655@gmail.com>
2018-04-13 21:53:34 +0530

    mongodb database added

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const express = require('express'),
-    cons = require('consolidate'),
-    mongooose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+// Use connect method to connect to the server
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    db.collection('movies').find({}).toArray(function(err, docs) {
+        if (err) {
+            console.log(err);
+        } else {
+            docs.forEach(function(doc) {
+                console.log(doc.title);
+            });
+        }
+        db.close();
+    });
+    console.log("entire data from database is displayed using official mongodb driver");
+});
