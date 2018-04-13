f2a3cff2303d053f7eecd8c3e92b423fbaea5d85
chandrakumarreddy <chandrapenugonda655@gmail.com>
2018-04-13 22:02:04 +0530

    express server stared using express module

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-const MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
-// Use connect method to connect to the server
-MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-    db.collection('movies').find({}).toArray(function(err, docs) {
-        if (err) {
-            console.log(err);
-        } else {
-            docs.forEach(function(doc) {
-                console.log(doc.title);
-            });
-        }
-        db.close();
-    });
-    console.log("entire data from database is displayed using official mongodb driver");
+const express = require('express'),
+    app = express();
+app.get('', function(req, res) {
+    res.send('Hello world');
 });
+app.use(function(req, res) {
+    res.sendStatus(404);
+});
+app.listen(3000);
