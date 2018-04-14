b2d23f06d8bc5fb4b3be3e14276dd364578f6cce
chandrakumarreddy <chandrapenugonda655@gmail.com>
2018-04-14 11:57:53 +0530

    Express:Handling get requests

--- app.js
+++ app.js
@@ -12,20 +12,10 @@ app.set('views', __dirname + '/views');
 ///////////
 ///routes
 //////////
-const url = 'mongodb://localhost:27017';
-// Database Name
-const dbName = 'video';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err);
-    const db = client.db(dbName);
-    app.get('/', function(req, res) {
-        db.collection('movies').find().toArray(function(err, movies) {
-            assert.equal(null, err);
-            res.render("home", {
-                'movies': movies
-            });
-        });
+app.get('/:name', function(req, res) {
+    res.render('home', {
+        'name': req.params.name,
+        'getval1': req.query.getval1
     });
 });
 /////////
